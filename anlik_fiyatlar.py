c7cbb9ccf9b9e0766875f365ae487fc7360ce3df
Efe Eroğlu <210541019@firat.edu.tr>
2024-04-21 23:07:00 +0300

    Graf Görselleştirmesi v1

--- anlik_fiyatlar.py
+++ anlik_fiyatlar.py
@@ -2,6 +2,8 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import json
+import matplotlib.pyplot as plt
 
 driver = webdriver.Chrome()
 
@@ -9,17 +11,25 @@ driver.get("https://www.getmidas.com/canli-borsa/")
 
 accept_cookies = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH,"/html/body/section/div/div/div/div/div[2]/button[1]")))
 accept_cookies.click()
-print("1")
 
 view_all_button = WebDriverWait(driver,10).until(EC.visibility_of_element_located((By.XPATH,"/html/body/div[1]/div/div/div/div[6]/div/button")))
 view_all_button.click()
 
 hisseler = []
+
+
 for i in range(1,555):
     hisse = driver.find_element(By.XPATH,f"/html/body/div[1]/div/div/div/div[6]/table/tbody/tr[{i}]/td[1]/a").text
     fiyat = driver.find_element(By.XPATH,f"/html/body/div[1]/div/div/div/div[6]/table/tbody/tr[{i}]/td[2]").text
     fark = driver.find_element(By.XPATH,f"/html/body/div[1]/div/div/div/div[6]/table/tbody/tr[{i}]/td[5]").text
 
-    print(f"{hisse} : {fiyat} Artış: {fark}")
+    hisseler.append({"Hisse": hisse, "Fiyat": fiyat, "Fark": fark})
+
+
+    print(hisseler)
+
+ # JSON dosyasına hisseleri kaydet
+with open("hisseler.json", "w") as json_file:
+    json.dump(hisseler, json_file)   
 
 driver.quit()
